ae278d59012c7f49aff6526e269a762690b33f72
rashidul <rashidul69@gmail.com>
2022-08-24 08:10:40 +0600

    fix website template asset link

--- src/Commands/templates/master.blade.php
+++ src/Commands/templates/master.blade.php
@@ -17,13 +17,13 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="/river/site/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="/river/site/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/river/site/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="/river/site/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="/river/site/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="/river/site/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-    <link href="/river/site/assets/css/style.css" rel="stylesheet">
+    <link href="/river/site/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="/river/site/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/river/site/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/river/site/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/river/site/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/river/site/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="/river/site/css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -39,14 +39,14 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="/river/site/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/river/site/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="/river/site/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="/river/site/assets/vendor/swiper/swiper-bundle.min.js"></script>
-{{--<script src="/river/site/assets/vendor/php-email-form/validate.js"></script>--}}
+<script src="/river/site/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/river/site/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/river/site/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/river/site/vendor/swiper/swiper-bundle.min.js"></script>
+{{--<script src="/river/site/vendor/php-email-form/validate.js"></script>--}}
 
 <!-- Template Main JS File -->
-<script src="/river/site/assets/js/main.js"></script>
+<script src="/river/site/js/main.js"></script>
 
 </body>
 
