42ccc60401f723f2d4e144189d63a56604ae5e9f
mahmudulhasan716 <mahmudul9944@gmail.com>
2024-01-25 12:08:34 +0600

    template-assets updated

--- resources/views/admin/template_assets/pages.blade.php
+++ resources/views/admin/template_assets/pages.blade.php
@@ -78,7 +78,7 @@
 
             <h3>Image Files</h3>
             <div class="col-md-12">
-                <div class="table-responsive">
+                {{-- <div class="table-responsive">
                     <table class="table table-vcenter">
                       <thead>
                         <tr>
@@ -99,8 +99,34 @@
                           </tr>
                         @endforeach
 
+
                       </tbody>
                     </table>
+                  </div> --}}
+
+
+                  <div>
+                    <div class="row">
+                      @foreach($image_file_name as $key=>$file)
+                      <div class="card mx-2 col-2 mt-5  ">
+                         <div class="card-body d-flex  align-content-between flex-wrap">
+
+                            <div class="mt-t " >
+                              <a> <img style="width:100px" src="/river/assets/{{ $file }}" /> {{$file}}</a>
+    
+                            </div>
+                            <div class="mt-2">
+                                <a href="#" class="btn btn-icon btn-copy-clipboard mx-3 "  data-url="/river/assets/{{$file}}" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Copied">
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>
+                              </a>
+                              <a class="btn btn-icon btn-delete" data-route="{{ route('river.template-assets.destroy',$file ) }}"> <i class="fas fa-trash-alt text-danger" ></i> </a>
+    
+                            </div>
+
+                         </div>   
+                      </div>
+                      @endforeach
+                    </div>
                   </div>
 
             </div>
